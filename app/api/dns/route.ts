b268bad5a06ffe8da1df79edad4398f6b77e5e4e
Arthur Ferreira <arthur.ferreiramiran@gmail.com>
2026-04-24 23:35:57 +0300

    add lookup history with Upstash Redis

--- app/api/dns/route.ts
+++ app/api/dns/route.ts
@@ -1,23 +1,31 @@
+import { redis } from "@/lib/redis-client"
+import { v4 as uuidv4 } from "uuid";
 
+export async function GET(request: Request) {
 
-
-export async function GET(request: Request){
-
-    const {searchParams} = new URL(request.url)
+    const { searchParams } = new URL(request.url)
 
     const domain = searchParams.get('domain')
     const domainType = searchParams.get('type')
 
 
-   
-    const res = await fetch(`${process.env.API_URL}?domain=${domain}&type=${domainType}`,{
 
-            headers: {
-                "x-api-key": process.env.API_GATEWAY_KEY ?? ""
-            }
-        })
+    const res = await fetch(`${process.env.API_URL}?domain=${domain}&type=${domainType}`, {
 
-        const data = await res.json()
-        return Response.json(data)
+        headers: {
+            "x-api-key": process.env.API_GATEWAY_KEY ?? ""
+        }
+    })
+
+    const data = await res.json()
+    const id = uuidv4()
+    await redis.set(`lookup:${id}`, {
+
+        domain,
+        type: domainType,
+        result: data,
+        timestamp: new Date().toISOString()
+    })
+    return Response.json(data)
 
 }
